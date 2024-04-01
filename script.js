dd3a19265c6e5a3d1c0029ec72a5d6ae73af969e
Mev2137 <szymongrysiewicz@gmail.com>
2024-04-01 21:03:42 +0200

    v1

---
+++ script.js
@@ -0,0 +1,110 @@
+
+const myLibrary = [
+];
+
+const book1 = new Book("The Lord of the Rings", "Tolkiem", 231,true)
+const book2 = new Book("One Piece", "Eiichiro Oda", 1200, true)
+myLibrary.push(book1)
+myLibrary.push(book2)
+const titleForm = document.querySelector("#title")
+const authorForm = document.querySelector("#author")
+const pagesForm = document.querySelector("#pages")
+const readForm = document.querySelector("#read")
+
+
+
+function Book(title,author,pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.toggleRead = function(){
+        this.read = !this.read;
+        showBooks()
+    }
+}
+function addBookToLibrary(event){
+    event.preventDefault();
+    title = titleForm.value
+    author = authorForm.value
+    pages = pagesForm.value
+    const isRead = readForm.checked;
+    const book = new Book(title,author,pages,isRead)
+    myLibrary.push(book)
+    showBooks()
+    
+}
+
+const submitButton = document.querySelector('#submitButton')
+submitButton.addEventListener('click', addBookToLibrary)
+
+const books= document.querySelector('#book-container');
+myLibrary.forEach(book => {
+    console.log(book)
+    
+});
+function showBooks(){
+    books.innerHTML = '';
+    myLibrary.forEach(book => {
+        const card = document.createElement("div")
+        card.classList.add("card")
+
+        title = document.createElement("p")
+        title.innerHTML = book.title
+
+        author = document.createElement("p")
+        author.innerHTML = book.author
+
+        pages = document.createElement("p")
+        pages.innerHTML = book.pages
+
+        read = document.createElement("p")    
+        read.innerHTML = book.read 
+        read.addEventListener('click',() => {
+            book.toggleRead()          
+            showBooks()
+            
+        })
+        
+
+        deleteButton = document.createElement("button")
+        deleteButton.innerHTML = "delete"
+        deleteButton.addEventListener('click',() => {
+            const index = myLibrary.indexOf(book);
+            if (index > -1) {
+                myLibrary.splice(index, 1); // Corrected this line
+            }
+            showBooks()
+        })
+
+        card.append(title)
+        card.append(author)
+        card.append(pages)
+        card.append(read)
+        card.append(deleteButton)
+        books.append(card)
+        })
+
+        
+        
+        
+    };
+    function openPopup(){
+        const popup = document.querySelector('#popup');
+        const backdrop = document.querySelector('#backdrop');
+        popup.classList.add("open-popup");
+        backdrop.style.display = 'block'; // Show the backdrop
+        backdrop.classList.add("blur"); // Blur the backdrop
+    }
+    
+    function closePopup(){
+        const popup = document.querySelector('#popup');
+        const backdrop = document.querySelector('#backdrop');
+        popup.classList.remove("open-popup");
+        backdrop.style.display = 'none'; // Hide the backdrop
+        backdrop.classList.remove("blur"); // Remove the blur
+    }
+window.onload = showBooks;
+
+
+
